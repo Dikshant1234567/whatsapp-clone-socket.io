72e3ce9fcc24bcb01c16a5c9dfd1d9cb2f729721
Dikshant <dikshantnaudiyal06@gmail.com>
2023-04-08 21:30:23 +0530

    socket.io server

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { Server } from "socket.io";
+const port = process.env.PORT || 9000
 
-const io = new Server(9000, {
+const io = new Server(port, {
   cors: {
     origin: "http://localhost:5173",
   },
